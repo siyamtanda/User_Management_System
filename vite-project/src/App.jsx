babeaf6123b0d137c9551deed8fff3ffa730d9a1
siyamtanda <68695019+siyamtanda@users.noreply.github.com>
2023-09-01 14:41:45 +0200

    CRUD

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
+import CreateUser from './CreateUser'
+
+
+function App() {
+
+  return (
+   <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Users/>}></Route>
+      <Route path='/create' element={<CreateUser/>}></Route>
+      <Route path='/update/:id' element={<UpdateUser/>}></Route>
+    </Routes>
+    </BrowserRouter>
+   </div>
+  )
+}
+
+export default App
